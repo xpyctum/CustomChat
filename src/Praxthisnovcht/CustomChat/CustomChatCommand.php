415a2635ae2c8f774322a84a6c19f3f4aa97251e
Praxthisnovcht <praxthisnovcht@gmail.com>
2014-12-11 03:37:49 +0000

    AntiGrief CustomChat optional v1.1.2

--- src/Praxthisnovcht/CustomChat/CustomChatCommand.php
+++ src/Praxthisnovcht/CustomChat/CustomChatCommand.php
@@ -68,7 +68,8 @@ class CustomChatCommand {
 	 * @return boolean
 	 */
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-		// disable chat for all players
+		if (!$this->hasCommandAccess($sender)) {
+			$sender->sendMessage ( TextFormat::RED . "You don't have permission to use this command" );		// disable chat for all players
 		if ((strtolower ( $command->getName () ) == "disablechat")) {
 			$this->pgin->getConfig ()->set ( "disablechat", true ); // config.yml
 			$this->pgin->getConfig ()->save ();
@@ -205,7 +206,31 @@ class CustomChatCommand {
 			
 		}
 	}
-	             // TODO NEXT VERSION
+	public function executeCommandKILL(CommandSender $sender, array $args) {	// CustomChat KillGrief v1..1.2			
+		if (!$this->hasCommandAccess($sender)) {
+			$sender->sendMessage (TextFormat::RED . "You don't have permission to use this command" );
+			return true;			
+		}		
+		if (count ( $args ) > 1) {
+			$sender->sendMessage (TextFormat::RED . "Usage: /grief [player]"  );
+			return true;
+		}
+		if (count ( $args ) === 1) {
+			// check if player online
+			$p = $this->getServerOnlinePlayer ( $sender, $args [0] );
+			// $p = $sender->getServer ()->getPlayer ( $args [0] );
+			if (is_null ( $p )) {
+				$msg = TextFormat::RED . "Cancelled Kill [" . $args [0] . "]. Player is offline.";
+				$sender->sendMessage ( $msg );
+				return true;
+			} else {
+				$p->sendMessage (TextFormat::RED . "You has been terminated!" );
+				$sender->sendMessage (TextFormat::RED . "Target Player [" . $args [0] . "] terminated." );
+				$p->kill ();
+			}
+		}
+		return true;
+	}	             // TODO NEXT VERSION
 	
 	private function hasCommandAccess(CommandSender $sender) {
 		if ($sender->getName () == "CONSOLE") {
