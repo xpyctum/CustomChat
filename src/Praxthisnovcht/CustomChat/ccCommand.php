e718a7e8466be9ea7a90896dd9ed98c494945d7e
Praxthisnovcht <praxthisnovcht@gmail.com>
2014-12-29 22:56:46 +0100

    Bump Version 1.2.5

--- src/Praxthisnovcht/CustomChat/ccCommand.php
+++ src/Praxthisnovcht/CustomChat/ccCommand.php
@@ -29,7 +29,7 @@ use pocketmine\network\protocol\Info;
 use pocketmine\network\protocol\LoginPacket;
 
 /**
- * Command // CustomChat 1.1.7 Release
+ * Command // CustomChat 1.2.5 Release
  *
  */
 class ccCommand {
